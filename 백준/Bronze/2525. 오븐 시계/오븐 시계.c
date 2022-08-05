80cb8921533587ea62d225ef9d40188a9ee1afac
Jhyun <59386798+dev-jhyun@users.noreply.github.com>
2022-08-05 23:00:04 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int h, m, t;
+    scanf("%d %d", &h, &m);
+    scanf("%d", &t);
+    
+    h = h + t/60;
+    m = m + t%60;
+    
+    if(m>=60){
+        h++;
+        m=m%60;
+    }
+    
+    if(h>=24){
+        h=h%24;
+    }
+    
+    
+    printf("%d %d", h,m);
+}
