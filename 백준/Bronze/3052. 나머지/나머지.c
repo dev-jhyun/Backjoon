2c642dbac81daa03a458dc3582ce8dca5750fbb5
Jhyun <59386798+jhyun96@users.noreply.github.com>
2022-08-10 00:18:44 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int A;
+    int arr[10] = {0,};
+    int result=0;
+
+    for(int i=0; i<10; i++){
+        scanf("%d", &A);
+        arr[i]=A%42;
+    }
+
+    for(int i=0; i<10; i++){
+        int count = 0;
+        for(int j=i+1; j<10; j++){
+            if(arr[i] == arr[j])
+                count++;
+        }
+        if(count==0)
+            result++;
+        
+    }
+
+    printf("%d",result);
+
+}
